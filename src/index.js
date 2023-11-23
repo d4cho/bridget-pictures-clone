1fa2030eed312b07b1faef4338d03b4c64e26144
d4cho <danielcho91@gmail.com>
2023-11-23 17:28:28 -0500

    add bio widget

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AppContextProvider } from './context/AppContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppContextProvider } from "./context/AppContext";
+import BioStorybookWidget from "bio-storybook-widget";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <AppContextProvider>
-            <App />
-        </AppContextProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AppContextProvider>
+      <BioStorybookWidget projectSource={"bridget-baker"} />
+      <App />
+    </AppContextProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
